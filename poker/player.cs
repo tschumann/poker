1bf026fbc85568d40e53cc847550673e07fd2818
Tom Schumann <schumann.tom@gmail.com>
2015-11-01 11:17:31 +1000

    Initial import.

---
+++ poker/player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+class Player : IPlayer
+{
+    private List<Card> hand;
+
+    public int bet()
+    {
+        return 0;
+    }
+
+    public Card play()
+    {
+        return null;
+    }
+}
