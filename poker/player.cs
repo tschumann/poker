b4609bdb086bcb9e630af9d5ef956d3c5d9a066d
Tom Schumann <schumann.tom@gmail.com>
2016-01-20 20:28:40 +1000

    Started adding getHandType method.

--- poker/player.cs
+++ poker/player.cs
@@ -2,6 +2,19 @@
 
 class Player : Card.AbstractCardPlayer
 {
+    public enum HandType
+    {
+        HIGH_CARD,
+        PAIR,
+        TWO_PAIR,
+        THREE_OF_A_KIND,
+        STRAIGHT,
+        FLUSH,
+        FULL_HOUSE,
+        FOUR_OF_A_KIND,
+        STRAIGHT_FLUSH,
+    }
+
     public int bet()
     {
         return 0;
@@ -10,5 +23,26 @@ class Player : Card.AbstractCardPlayer
     public override Card.Card play()
     {
         return null;
+    }
+
+    public HandType getHandType()
+    {
+        bool allSameSuit = false;
+        bool allInOrder = false;
+
+        if (allSameSuit && allInOrder)
+        {
+            return HandType.STRAIGHT_FLUSH;
+        }
+        if (allSameSuit && !allInOrder)
+        {
+            return HandType.FLUSH;
+        }
+        if (!allSameSuit && allInOrder)
+        {
+            return HandType.STRAIGHT;
+        }
+
+        return HandType.HIGH_CARD;
     }
 }
