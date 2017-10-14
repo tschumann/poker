cc4b51175d9da5758bee288fbe33dded86918976
tschumann <schumann.tom@gmail.com>
2017-10-14 17:28:22 +1000

    Started adding dealing.

--- poker/game.cs
+++ poker/game.cs
@@ -6,11 +6,23 @@ namespace poker
 {
     public class Game : Card.IGame
     {
-        protected List<ICardPlayer> players;
+        private List<ICardPlayer> players;
+        private Deck deck;
 
         public Game()
         {
             this.players = new List<ICardPlayer>();
+            this.deck = new Deck(this, false);
+        }
+
+        public void deal()
+        {
+            this.deck.shuffle();
+
+            foreach (var player in this.players)
+            {
+                player.receive(this.deck.draw());
+            }
         }
         
         static void Main(string[] args)
